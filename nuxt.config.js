7787dfd7458d4c76691faaca44c8ae9c120961e5
zoey.xu <39@klnnn.com>
2021-08-13 16:59:09 +0800

    static mode

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,8 @@ export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: true,
 
+  target: 'static',
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'MKT Blog',
